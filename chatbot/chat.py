e68593ec0fbce5a1c6f07955884c496dae88b11f
Abhishek <abhishekmalhotra1292@gmail.com>
2023-08-09 01:19:14 +0530

    Deployment using flask

--- chatbot/chat.py
+++ chatbot/chat.py
@@ -25,15 +25,8 @@ model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Eliii"
-print("Let's chat! (type 'quit' to exit)")
-while True:
-    # sentence = "do you use credit cards?"
-    sentence = input("You: ")
-    if sentence == "quit":
-        break
-
-    sentence = tokenize(sentence)
+def getresponse(msg):
+    sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
     X = torch.from_numpy(X).to(device)
@@ -48,6 +41,18 @@ while True:
     if prob.item() > 0.80:
         for intent in intents['intents']:
             if tag == intent["tag"]:
-                print(f"{bot_name}: {random.choice(intent['responses'])}")
+                return random.choice(intent['responses'])
+                
     else:
-        print(f"{bot_name}: I do not understand...")
+        return "I do not understand..."
+
+
+
+if __name__=="__main__":
+    bot_name = "Eliii"
+    print("Let's chat! (type 'quit' to exit)")
+    while True:
+        msg = input("You: ")
+        if msg == "quit":
+            break
+        print(f"{bot_name}: {getresponse(msg)}")
